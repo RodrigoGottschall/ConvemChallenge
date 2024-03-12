f9641119cd5006f42e190c71f55b1ff25d791ee4
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-03-12 08:26:35 -0300

    aws initial config

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const AWS = require("aws-sdk");
 
 const app = express();
 
-// Usar o body-parser para analisar JSON
 app.use(bodyParser.json());
 
-// Definir a rota POST
+// Configure suas credenciais AWS
+AWS.config.update({
+  accessKeyId: "SUA_ACCESS_KEY_ID",
+  secretAccessKey: "SUA_SECRET_ACCESS_KEY",
+  region: "SUA_REGIÃO", // ex: us-east-1
+});
+
+const sqs = new AWS.SQS();
+
 app.post("/transactions", (req, res) => {
-  // Obter o payload da requisição
   const { idempotencyId, amount, type } = req.body;
 
   // Validar o payload (opcional)
 
-  // Processar a transação
-  // ...
+  // Criar mensagem SQS
+  const message = {
+    Body: JSON.stringify(req.body),
+    QueueUrl: "URL_DA_SUA_FILA_SQS",
+  };
 
-  // Retornar uma resposta
-  res.status(201).send({ message: "Transação criada com sucesso!" });
+  // Enviar mensagem para a fila
+  sqs.sendMessage(message, (err, data) => {
+    if (err) {
+      console.error("Erro ao enviar mensagem para SQS:", err);
+      res.status(500).send({ message: "Erro ao processar transação" });
+    } else {
+      console.log("Mensagem enviada para SQS:", data.MessageId);
+      res.status(201).send({ message: "Transação criada com sucesso!" });
+    }
+  });
 });
 
-// Iniciar o servidor
 app.listen(3000, () => {
   console.log("Servidor em execução na porta 3000");
 });
